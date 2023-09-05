8812ce8f7100f72eb4c7f0fd0905416bd8358414
hanting-cong <hanting.cong@hulu.com>
2023-09-05 14:48:31 +0800

    first commit

---
+++ DataProcessing/data_convertor.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Author: Cong
+# Date: 2023-09-05
+
+import json
+
+KEY_TYPE = "type"
+KEY_INSIDE = "inside"
+
+
+class DataConvertor:
+
+    def __init__(self):
+        return
+
+    @staticmethod
+    def get_raw_objs(source_path):
+        raw_objs = []
+        with open(source_path, mode="r") as f:
+            for line in f:
+                raw_data_obj = RawDataObj(line)
+                raw_objs.append(raw_data_obj)
+        return raw_objs
+
+
+class RawDataObj:
+    def __init__(self, type, inside):
+        self.type = type
+        self.inside = inside
+
+    def __init__(self, json_str):
+        json_obj = json.loads(json_str, strict=False)
+        self.type = json_obj.get(KEY_TYPE)
+        self.inside = json_obj.get(KEY_INSIDE)
+
+    def get_type(self):
+        return self.type
+
+    def get_inside(self):
+        return self.inside
---
+++ DataProcessing/lm_data_convertor.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Author: Cong
+# Date: 2023-09-05
+
+import json
+from data_convertor import DataConvertor, RawDataObj
+
+
+class LMDataConvertor(DataConvertor):
+    def __int__(self):
+        return
+
+    def convert_to_text_only(self, src_path, dst_path):
+        lm_data_instances = []
+        raw_data_objs = self.get_raw_objs(src_path)
+        for raw_data_obj in raw_data_objs:
+            lm_text_only_obj = {"text": raw_data_obj.get_inside()}
+            lm_data_instances.append(lm_text_only_obj)
+
+        with open(dst_path, mode="w", encoding="utf8") as f:
+            f.write(json.dumps(LMDataSet("text_only", lm_data_instances).to_json(), ensure_ascii=False))
+
+
+class LMDataSet:
+    def __init__(self, data_type, instances):
+        self.type = data_type
+        self.instances = instances
+
+    def to_json(self):
+        return json.dumps(
+            {
+                "type": self.type,
+                "instances": self.instances
+            },
+            ensure_ascii=False
+        )
